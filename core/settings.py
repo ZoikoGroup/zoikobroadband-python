ebd7889935e5d206508fef0b1cc552922f2f6c15
sumonklyn <sumonklyn@gmail.com>
2026-05-06 15:32:29 +0530

    fixing

--- core/settings.py
+++ core/settings.py
@@ -186,6 +186,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
     "https://zoikobroadband-python.vercel.app",
     "http://34.147.150.254",
     "https://api.zoikobroadband.com",
