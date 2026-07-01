1faa829b995eff429f0495c08bf0c0ac3f5e7adb
sumonklyn <sumonklyn@gmail.com>
2026-07-01 18:42:22 +0530

    email 2

--- core/settings.py
+++ core/settings.py
@@ -242,8 +242,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtpout.secureserver.net"
 EMAIL_PORT = 587
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = "info@zoikoorbit.com"
 EMAIL_HOST_PASSWORD = "NoxxMC26070%!LGM"
